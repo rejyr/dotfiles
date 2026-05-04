1e60b180175201acabc4a94e5fc9a222cb2e43b2
Jerry Wang <jerrylwang123@gmail.com>
2026-05-03 20:03:19 -0400

    feat(mpd): working mpd

--- nixos/modules/features/homeModules/mpd/mpd.nix
+++ nixos/modules/features/homeModules/mpd/mpd.nix
@@ -15,19 +15,7 @@
       services.mpd = {
         enable = true;
         musicDirectory = "~/Music";
-        extraConfig = ''
-          auto_update "yes"
-          audio_output {
-            type "pipewire"
-            name "My PipeWire Output"
-          }
-          audio_output {
-            type "fifo"
-            name "my_fifo"
-            path "/tmp/mpd.fifo"
-            format "44100:16:2"
-          }
-        '';
+        dbFile = "~/.mpd/database";
       };
       services.mpdris2-rs.enable = true;
       xdg.configFile.rmpc.source = ./rmpc;
