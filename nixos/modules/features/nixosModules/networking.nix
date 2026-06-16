4ab842a90e82ccd12db2a646b42ef077edf2dec3
Jerry Wang <jerrylwang123@gmail.com>
2026-06-16 14:53:05 -0400

    fix: noctalia nixos dependencies

--- nixos/modules/features/nixosModules/networking.nix
+++ nixos/modules/features/nixosModules/networking.nix
@@ -20,11 +20,7 @@
       };
 
       config = lib.mkIf cfg.enable {
-        networking.wireless.iwd.enable = true;
-
-        environment.systemPackages = with pkgs; [
-          impala
-        ];
+        networking.networkmanager.enable = true;
       };
     };
 }
--- nixos/modules/features/nixosModules/powerManagement.nix
+++ nixos/modules/features/nixosModules/powerManagement.nix
@@ -21,18 +21,8 @@
 
       config = lib.mkIf cfg.enable {
         powerManagement.enable = true;
-        services.tlp = {
-          enable = true;
-          settings = {
-            CPU_SCALING_GOVERNOR_ON_AC = "performance";
-            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-            CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-            CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-            START_CHARGE_THRESH_BAT0 = 40;
-            STOP_CHARGE_THRESH_BAT0 = 80;
-          };
-        };
+        services.tuned.enable = true;
+        services.upower.enable = true;
       };
     };
 }
